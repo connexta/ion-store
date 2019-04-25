148d03381744586e91c01b79a2eecc7579686741
Josh Hunziker <joshua.hunziker@connexta.com>
2019-04-25 15:36:28 -0700

    Update gradle files, java version, and add an initial application package

---
+++ src/main/java/com/connexta/multiintstore/MultiIntStoreApplication.java
@@ -0,0 +1,13 @@
+package com.connexta.multiintstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiIntStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiIntStoreApplication.class, args);
+	}
+
+}
