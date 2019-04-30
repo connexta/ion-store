20886ef158aad28f65c315d659a8528cf38278df
Josh Hunziker <jhunzik@users.noreply.github.com>
2019-04-30 08:21:14 -0700

    Added the initial structure for the MIS (#3)
    * Update gradle files, java version, and add an initial application package
    
    * Added error handlers and controllers
    
    * Added the copyright to all the new files
    
    * Add jdk11 to jenkins tools

---
+++ src/main/java/com/multiintstore/MultiIntStoreApplication.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Connexta
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the
+ * GNU Lesser General Public License is distributed along with this
+ * program and can be found at http://www.gnu.org/licenses/lgpl.html.
+ */
+
+package com.multiintstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiIntStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiIntStoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/multiintstore/storage/persistence/ProductDao.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Connexta
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the
+ * GNU Lesser General Public License is distributed along with this
+ * program and can be found at http://www.gnu.org/licenses/lgpl.html.
+ */
+
+package com.multiintstore.storage.persistence;
+
+import com.multiintstore.storage.persistence.models.Product;
+
+import java.util.Optional;
+
+public class ProductDao implements Dao<Product> {
+    @Override
+    public Optional get(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(Product product) {
+
+    }
+
+    @Override
+    public void delete(Product product) {
+
+    }
+}
