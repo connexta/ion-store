a178c6ff9e9198289c59712d4c354d7a6eebd240
Kjames5269 <kjames5269@gmail.com>
2019-05-16 08:41:24 -0700

    Running the formatter

--- search/src/main/java/com/connexta/search/impl/VisitorImpl.java
+++ search/src/main/java/com/connexta/search/impl/VisitorImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) Connexta
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the
+ * GNU Lesser General Public License is distributed along with this
+ * program and can be found at http://www.gnu.org/licenses/lgpl.html.
+ */
 package com.connexta.search.impl;
 
 import com.connexta.search.api.Combiner;
@@ -6,22 +21,14 @@ import com.connexta.search.api.Visitor;
 public class VisitorImpl implements Visitor {
 
   @Override
-  public void addAttribute(String attribute) {
-
-  }
+  public void addAttribute(String attribute) {}
 
   @Override
-  public void addOperator(String operator) {
-
-  }
+  public void addOperator(String operator) {}
 
   @Override
-  public void addValue(String value) {
-
-  }
+  public void addValue(String value) {}
 
   @Override
-  public void addCombiner(Combiner combiner) {
-
-  }
+  public void addCombiner(Combiner combiner) {}
 }
