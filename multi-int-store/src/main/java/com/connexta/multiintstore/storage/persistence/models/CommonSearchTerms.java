75e1bec1f3243bbfbefea6f98f47b3a4bc26fb09
Josh Hunziker <jhunzik@users.noreply.github.com>
2019-06-04 11:13:55 -0700

    Update license for java files (#22)

--- multi-int-store/src/main/java/com/connexta/multiintstore/storage/persistence/models/CommonSearchTerms.java
+++ multi-int-store/src/main/java/com/connexta/multiintstore/storage/persistence/models/CommonSearchTerms.java
@@ -1,17 +1,8 @@
 /*
- * Copyright (c) Connexta
+ * Copyright (c) 2019 Connexta, LLC
  *
- * This is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details. A copy of the
- * GNU Lesser General Public License is distributed along with this
- * program and can be found at http://www.gnu.org/licenses/lgpl.html.
+ * Released under the GNU Lesser General Public License version 3; see
+ * https://www.gnu.org/licenses/lgpl-3.0.html
  */
 package com.connexta.multiintstore.storage.persistence.models;
 
