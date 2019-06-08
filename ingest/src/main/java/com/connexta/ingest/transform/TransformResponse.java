9e819c4df5e35b6c01361a098af20cf9224eb1aa
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-06-08 00:23:15 -0700

    OHANA-122    apply spotless

--- ingest/src/main/java/com/connexta/ingest/transform/TransformResponse.java
+++ ingest/src/main/java/com/connexta/ingest/transform/TransformResponse.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019 Connexta, LLC
+ *
+ * Released under the GNU Lesser General Public License version 3; see
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ */
 package com.connexta.ingest.transform;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
