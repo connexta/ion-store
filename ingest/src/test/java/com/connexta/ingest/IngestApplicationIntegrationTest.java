f96f28f2ef1650ab3ea24c64ec5a968ae7b77066
Kjames5269 <kjames5269@gmail.com>
2019-06-11 10:31:40 -0700

    Adding itests for the ingest api

--- ingest/src/test/java/com/connexta/ingest/IngestApplicationIntegrationTest.java
+++ ingest/src/test/java/com/connexta/ingest/IngestApplicationIntegrationTest.java
@@ -6,13 +6,58 @@
  */
 package com.connexta.ingest;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class IngestApplicationIntegrationTest {
 
+  @Autowired
+  WebApplicationContext wac;
+
+  private MockMvc mvc;
+
+  @Before
+  public void beforeEach() {
+    mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+  }
+
   @Test
-  public void contextLoads() {}
+  public void correctlyFormattedIngestRequestTest() throws Exception {
+
+    mvc.perform(multipart("/ingest").file("file", "some-content".getBytes())
+                    .param("fileSize", "10")
+                    .param("filename", "file")
+                    .param("title", "qualityTitle")
+                    .param("mimeType", "plain/text")
+                    .header("Accept-Version", "1.2.1")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.MULTIPART_FORM_DATA))
+        .andExpect(status().isNotImplemented());
+  }
+
+  @Test
+  public void incorrectlyFormattedIngestRequestTest() throws Exception {
+
+    mvc.perform(multipart("/ingest").file("file", "some-content".getBytes())
+                    .param("filename", "file")
+                    .param("title", "qualityTitle")
+                    .param("mimeType", "plain/text")
+                    .header("Accept-Version", "1.2.1")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.MULTIPART_FORM_DATA))
+        .andExpect(status().is4xxClientError());
+  }
 }
