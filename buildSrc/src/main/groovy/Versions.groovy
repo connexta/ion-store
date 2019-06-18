05de04fd9c75cfc8a072154d6dff8d8e7b42523b
Kjames5269 <kjames5269@gmail.com>
2019-06-18 08:46:59 -0700

    Updating Gradle files :: pre-spotless

---
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 Connexta, LLC
+ *
+ * Released under the GNU Lesser General Public License version 3; see
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ */
+/* Default Package */
+class Versions {
+
+    static String java = "1.11"
+    static String project = "0.1.0-SNAPSHOT"
+
+    //  Versions
+    static String awsBom = "2.5.29"
+    static String commonsIO = "2.6"
+    static String guava = "27.1-jre"
+    static String ingestAPI = "0.1.1"
+    static String jacksonCore = "2.9.9"
+    static String json = "20180813"
+    static String lombok = "1.18.6"
+    static String springBoot = "2.1.5.RELEASE"
+    static String springData = "4.0.8.RELEASE"
+    static String tomcat = "9.0.19"
+    static String transformAPI = "0.0.1-SNAPSHOT"
+
+    //  Test Versions
+    static String hamcrest = "2.0.0"
+    static String junit = "4.12"
+    static String mockito = "2.23.0"
+    static String restito = "0.9.3"
+    static String testContainers = "1.11.2"
+
+    //  Plugin Versions
+    static String palantir = "0.22.1"
+
+}
