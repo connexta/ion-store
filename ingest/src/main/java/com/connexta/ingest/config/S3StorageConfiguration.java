65a30fde9410d06a3e097f84d26fd5ad6a2177c5
Josh Hunziker <jhunzik@users.noreply.github.com>
2019-06-19 11:24:34 -0700

    Make the S3Client a bean (#56)

--- ingest/src/main/java/com/connexta/ingest/config/S3StorageConfiguration.java
+++ ingest/src/main/java/com/connexta/ingest/config/S3StorageConfiguration.java
@@ -8,20 +8,27 @@ package com.connexta.ingest.config;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import javax.annotation.PostConstruct;
-import lombok.Getter;
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-@ComponentScan
-@Getter
 @Profile("default")
 public class S3StorageConfiguration {
+
+  Logger LOGGER = LoggerFactory.getLogger(S3StorageConfiguration.class);
 
   @Value("${aws.s3.endpointUrl}")
   private String s3Endpoint;
@@ -42,8 +49,29 @@ public class S3StorageConfiguration {
   @Value("${aws.s3.bucket.quarantine}")
   private String s3BucketQuarantine;
 
+  @Bean
+  public S3Client s3ClientFactory() {
+    AwsCredentials credentials = AwsBasicCredentials.create(s3AccessKey, s3SecretKey);
+
+    S3Client s3Client =
+        S3Client.builder()
+            .endpointOverride(URI.create(s3Endpoint))
+            .region(Region.of(s3Region))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build();
+    LOGGER.info("S3 Client has been initialized.");
+    LOGGER.info("Region: {}", s3Region);
+    LOGGER.info("Endpoint: {}", s3Endpoint);
+    return s3Client;
+  }
+
+  @Bean
+  public String s3BucketQuarantine() {
+    return s3BucketQuarantine;
+  }
+
   @PostConstruct
-  public void initialize() throws IOException {
+  public void readKeysFromFiles() throws IOException {
     s3AccessKey = FileUtils.readFileToString(new File(awsAccessKeyFile), StandardCharsets.UTF_8);
     s3SecretKey = FileUtils.readFileToString(new File(awsSecretKeyFile), StandardCharsets.UTF_8);
   }
--- ingest/src/test/java/com/connexta/ingest/TestUtilConfig.java
+++ ingest/src/test/java/com/connexta/ingest/TestUtilConfig.java
@@ -6,31 +6,23 @@
  */
 package com.connexta.ingest;
 
-import static org.mockito.Mockito.when;
-
-import com.connexta.ingest.config.S3StorageConfiguration;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 @Profile("test")
 public class TestUtilConfig {
 
   @Bean
-  @Primary
-  public S3StorageConfiguration getStorageConfig() {
+  public S3Client s3Client() {
+    return Mockito.mock(S3Client.class);
+  }
 
-    S3StorageConfiguration storageConfig = Mockito.mock(S3StorageConfiguration.class);
-
-    when(storageConfig.getS3AccessKey()).thenReturn("password");
-    when(storageConfig.getS3SecretKey()).thenReturn("password");
-    when(storageConfig.getS3Endpoint()).thenReturn("http://foobar.me/1234");
-    when(storageConfig.getS3Region()).thenReturn("us-west-1");
-    when(storageConfig.getS3BucketQuarantine()).thenReturn("Leaky");
-
-    return storageConfig;
+  @Bean
+  public String s3BucketQuarantine() {
+    return "test-bucket";
   }
 }
