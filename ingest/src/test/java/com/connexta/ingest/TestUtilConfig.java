08caf5efbb022cb9d3fb78e301712a6d725750c9
Josh Hunziker <joshua.hunziker@connexta.com>
2019-06-19 09:05:06 -0700

    Update Ingest Test beans

--- ingest/src/test/java/com/connexta/ingest/TestUtilConfig.java
+++ ingest/src/test/java/com/connexta/ingest/TestUtilConfig.java
@@ -14,10 +14,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 @Profile("test")
 public class TestUtilConfig {
+
+  @Bean
+  public S3Client s3Client() {
+    return Mockito.mock(S3Client.class);
+  }
+
+  @Bean
+  public String s3BucketQuarantine() {
+    return "test-bucket";
+  }
 
   @Bean
   @Primary
