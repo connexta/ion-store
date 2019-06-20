9276fdd57a8eac70fa9a03bfb34f4361f81fc1f3
Emily Berk <emmberk@users.noreply.github.com>
2019-06-20 14:16:31 -0700

    Upgraded dependency versions to latest release version (#61)

--- buildSrc/src/main/groovy/Versions.groovy
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -11,25 +11,25 @@ class Versions {
     static String project = "0.1.0-SNAPSHOT"
 
     //  Versions
-    static String awsBom = "2.5.29"
+    static String aws = "2.5.67"
     static String commonsIO = "2.6"
-    static String guava = "27.1-jre"
+    static String guava = "28.0-jre"
     static String ingestAPI = "0.1.1"
     static String jacksonCore = "2.9.9"
     static String json = "20180813"
-    static String lombok = "1.18.6"
-    static String springBoot = "2.1.5.RELEASE"
+    static String lombok = "1.18.8"
+    static String springBoot = "2.1.6.RELEASE"
     static String springData = "4.0.9.RELEASE"
-    static String tomcat = "9.0.19"
+    static String tomcat = "9.0.21"
     static String transformAPI = "0.0.1-SNAPSHOT"
     static String zookeeper = "3.5.5"
 
     //  Test Versions
     static String hamcrest = "2.0.0"
     static String junit = "4.12"
-    static String mockito = "2.23.0"
+    static String mockito = "2.28.2"
     static String restito = "0.9.3"
-    static String testContainers = "1.11.2"
+    static String testContainers = "1.11.3"
 
     //  Plugin Versions
     static String palantir = "0.22.1"
