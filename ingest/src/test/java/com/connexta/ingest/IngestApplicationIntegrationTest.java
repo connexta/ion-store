b9b8840a1a3e74d78d666ac6a43fb8dec83740bd
Vina Martin <vina.martin@connexta.com>
2019-06-20 09:58:49 -0700

    Mocks RestTemplate for ingest test (#59)

--- ingest/src/test/java/com/connexta/ingest/IngestApplicationIntegrationTest.java
+++ ingest/src/test/java/com/connexta/ingest/IngestApplicationIntegrationTest.java
@@ -6,20 +6,28 @@
  */
 package com.connexta.ingest;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.connexta.transformation.rest.models.TransformResponse;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -28,9 +36,11 @@ import software.amazon.awssdk.services.s3.S3Client;
 @ActiveProfiles("test")
 public class IngestApplicationIntegrationTest {
 
-  @Autowired WebApplicationContext wac;
-
   @Autowired S3Client mockS3Client;
+
+  @Autowired RestTemplate restTemplate;
+
+  @Autowired WebApplicationContext wac;
 
   private MockMvc mvc;
 
@@ -41,6 +51,33 @@ public class IngestApplicationIntegrationTest {
 
   @Test
   public void testContextLoads() {}
+
+  @Test
+  public void testSuccessfulTransformRequest() throws Exception {
+    when(restTemplate.postForObject(
+            eq("https://localhost/transform"), any(HttpEntity.class), eq(TransformResponse.class)))
+        .thenReturn(new TransformResponse());
+
+    mvc.perform(
+            multipart("/ingest")
+                .file("file", "some-content".getBytes())
+                .param("fileSize", "10")
+                .param("fileName", "file")
+                .param("title", "qualityTitle")
+                .param("mimeType", "plain/text")
+                .header("Accept-Version", "1.2.1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+        .andExpect(status().isOk());
+
+    verify(restTemplate, times(1))
+        .postForObject(
+            eq("https://localhost/transform"), any(HttpEntity.class), eq(TransformResponse.class));
+  }
+
+  // TODO Test what happens when an error occurs when hitting transform service
+  @Test
+  public void testUnsuccessfulRequest() {}
 
   @Ignore("TODO Add a stub S3 server or expect that the status is not 400 ")
   @Test
