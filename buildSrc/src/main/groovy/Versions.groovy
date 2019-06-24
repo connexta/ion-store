b687818ff4b34fb9a7c59d2cf649bc4f25c8a97a
Emily Berk <emmberk@users.noreply.github.com>
2019-06-24 15:38:04 -0700

    Cleaned up dependencies now that not every subproject is a spring-boot app (#66)
    * Addressed CVE-2019-12814
    * Disabled Sonatype OSS Index Analyzer in the org.owasp.dependencycheck plugin because there is a bug in version 5.0.0

--- buildSrc/src/main/groovy/Versions.groovy
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -11,27 +11,24 @@ class Versions {
     static String project = "0.1.0-SNAPSHOT"
 
     //  Versions
-    static String aws = "2.5.67"
+    static String aws = "2.5.69"
     static String commonsIO = "2.6"
-    static String guava = "28.0-jre"
     static String ingestAPI = "0.1.1"
-    static String jacksonCore = "2.9.9"
-    static String json = "20180813"
     static String lombok = "1.18.8"
     static String springBoot = "2.1.6.RELEASE"
     static String springData = "4.0.9.RELEASE"
-    static String tomcat = "9.0.21"
     static String transformAPI = "0.0.1-SNAPSHOT"
-    static String zookeeper = "3.5.5"
 
     //  Test Versions
-    static String hamcrest = "2.0.0"
     static String junit = "4.12"
     static String mockito = "2.28.2"
+    static String npathai = "2.0.0"
     static String restito = "0.9.3"
     static String testContainers = "1.11.3"
 
     //  Plugin Versions
     static String palantir = "0.22.1"
 
+    // OWASP Override Versions
+    static String zookeeper = "3.5.5"
 }
