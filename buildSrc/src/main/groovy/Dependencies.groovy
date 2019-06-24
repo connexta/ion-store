970dcb2dcb8f4e5c50586d341be7264bb0e402f7
Emily Berk <emily.m.berk@gmail.com>
2019-06-24 09:00:10 -0700

    Fixed alphabetical order of Dependencies

--- buildSrc/src/main/groovy/Dependencies.groovy
+++ buildSrc/src/main/groovy/Dependencies.groovy
@@ -9,9 +9,9 @@ class Dependencies {
 
   //  Dependencies
   static def awsS3 = { it=Versions.aws -> "software.amazon.awssdk:s3:${it}" }
-  static def lombok = { it=Versions.lombok -> "org.projectlombok:lombok:${it}" }
   static def commonsIO = { it=Versions.commonsIO -> "commons-io:commons-io:${it}" }
   static def ingestAPI = { it=Versions.ingestAPI -> "com.connexta.ingest:ingest-api-rest-spring-stubs:${it}" }
+  static def lombok = { it=Versions.lombok -> "org.projectlombok:lombok:${it}" }
   static def springBootStarterWeb = { it=Versions.springBoot -> "org.springframework.boot:spring-boot-starter-web:${it}" }
   static def springDataSolr = { it=Versions.springData -> "org.springframework.data:spring-data-solr:${it}" }
   static def transformAPI = { it=Versions.transformAPI -> "com.connexta.transformation:transformation-api-rest-spring-stubs:${it}" }
@@ -20,8 +20,8 @@ class Dependencies {
   static def hamcrestOptional = { it=Versions.npathai -> "com.github.npathai:hamcrest-optional:${it}" }
   static def junit = { it=Versions.junit -> "junit:junit:${it}" }
   static def mockito = { it= Versions.mockito -> "org.mockito:mockito-core:${it}" }
-  static def springBootStarterTest = { it=Versions.springBoot -> "org.springframework.boot:spring-boot-starter-test:${it}" }
   static def restito = { it=Versions.restito -> "com.xebialabs.restito:restito:${it}" }
+  static def springBootStarterTest = { it=Versions.springBoot -> "org.springframework.boot:spring-boot-starter-test:${it}" }
   static def testContainers = { it=Versions.testContainers -> "org.testcontainers:testcontainers:${it}" }
 
   //  Plugin Dependencies
