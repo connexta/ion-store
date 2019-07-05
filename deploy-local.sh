51a2cc86559ff2e5ea523b44aaa54944901d21ba
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-07-05 10:34:47 -0700

    Works on linux

--- deploy-local.sh
+++ deploy-local.sh
@@ -9,59 +9,62 @@ name=cdr
 # Evaluate the first parameter passed to the function every 2 seconds.
 # When the expression evaluates to an empty string, return.
 function wait_for_empty_results() {
-    while [[ ! -z  $(eval $1) ]]; do
-        sleep 2
-        echo $(eval $1)
-        printf "."
-    done
+  sleep 1
+  while [[ ! -z  $(eval $1) ]]; do
+    sleep 1
+    printf "." >& 2
+  done
 }
 
 # Secrets and configs are immutable. Docker swarm reports error "only updates to Labels are allowed"
 # when attempting to modify their values. Remove any existing configs and secrets before
 # attempting to create new ones.
-function clean () {
-    # Removing the stack removes its configs and secrets and that allows their values to be changed (recreated)
-    # Ignore err message if stack does not exist
-    docker stack rm $name 2> /dev/null
+function remove_stack () {
+  # Removing the stack removes its configs and secrets and that allows their values to be changed (recreated)
+  # Ignore err message if stack does not exist
+  docker stack rm $name 2&> /dev/null
 }
 
+# Return true if the network $name exists
+function network_exists() {
+  # Match only on the exact name (-w for whole word)
+  result=$(docker network ls | grep -w $name)
+  if [[ ! -z $result ]];
+    then
+      return 0
+    else
+      return 1
+  fi
+}
 function recreate_network () {
-    # Remove and recreate network in case it has the wrong options
-    docker network rm $name 2&> /dev/null
-    if [ $? -eq 0 ]
-       then
-          # RC 0 means there was a network to remove. Wait for it for it to happen.
-          echo "Waiting to remove network $name"
-          wait_for_empty_results "docker network ls | grep -w $name"
-   fi
+  if network_exists
+    then
+      printf "Removing network $name"
+      docker network rm $name > /dev/null
+      wait_for_empty_results "docker network ls | grep -w $name" >&2
+      echo ""
+  fi
 
-  docker network ls
-    # Once network is gone, recreate it
-    docker network create --driver=overlay --attachable $name > /dev/null
-    echo "----------------------"
-      docker network ls
+  # Once network is gone, recreate it
+  docker network create --driver=overlay --attachable $name > /dev/null
 
-
-    exit
 }
 
 # Deploy docker-compose.yml file via docker stacks
-function deploy_images () {
-    docker stack deploy -c docker-compose.yml -c docker-compose-override-local.yml $name
+function deploy_stack () {
+  docker stack deploy -c docker-compose.yml -c docker-compose-override-local.yml $name
 }
 
 function wait_for_services () {
-#set -x
-#trap read debug
-    printf "Waiting for docker services to start"
-    wait_for_empty_results "docker service ls | grep $name_.*0/[[:digit:]]"\
-    echo "Services started"
+  printf "Starting services"
+  wait_for_empty_results "docker service ls | grep $name_.*0/[[:digit:]]"\
+  echo ""
 }
 
 # ----- BEGIN -----
-clean
+remove_stack
 recreate_network
-deploy_images
+deploy_stack
 wait_for_services
 echo Exiting
 # ------ END ------
