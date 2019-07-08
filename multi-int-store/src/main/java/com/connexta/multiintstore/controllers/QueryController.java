f5cf4e96e1a22322c8f6642db04343c92ae6b6c2
Emily Berk <emily.m.berk@gmail.com>
2019-07-08 11:33:02 -0700

    Added an ExceptionHandler

--- multi-int-store/src/main/java/com/connexta/multiintstore/controllers/QueryController.java
+++ multi-int-store/src/main/java/com/connexta/multiintstore/controllers/QueryController.java
@@ -10,36 +10,40 @@ import com.connexta.multiintstore.exception.SearchException;
 import com.connexta.multiintstore.services.api.SearchService;
 import java.net.URL;
 import java.util.List;
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestController
 @RequestMapping(value = "/search")
 @AllArgsConstructor
+@Validated
 public class QueryController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(QueryController.class);
 
   private SearchService searchService;
 
-  @RequestMapping(method = RequestMethod.GET, params = "q")
+  @GetMapping
   @ResponseBody
-  public ResponseEntity<List<URL>> searchKeyword(@RequestParam(value = "q") String keyword) {
-    if (StringUtils.isEmpty(keyword)) {
-      final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-      LOGGER.warn(String.format("Unable to search for empty keyword. Returning %s.", httpStatus));
-      return new ResponseEntity<>(httpStatus);
-    }
-
+  public ResponseEntity<List<URL>> searchKeyword(
+      @RequestParam(value = "q") @NotEmpty final String keyword) {
     try {
       return new ResponseEntity<>(searchService.find(keyword), HttpStatus.OK);
     } catch (SearchException e) {
@@ -47,5 +51,18 @@ public class QueryController {
     }
 
     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ControllerAdvice
+  private class ConstraintViolationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConstraintViolation(
+        @NotNull final ConstraintViolationException e, @NotNull final WebRequest request) {
+      final String message = e.getMessage();
+      final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+      LOGGER.warn("Request is invalid: {}. Returning {}.", message, httpStatus, e);
+      return handleExceptionInternal(e, message, new HttpHeaders(), httpStatus, request);
+    }
   }
 }
