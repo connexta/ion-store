fe160493a5f39c6210924822a53d63e5c0083ed2
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-07-12 16:57:07 -0700

    Actuator for ingest and MIS services

--- buildSrc/src/main/groovy/Dependencies.groovy
+++ buildSrc/src/main/groovy/Dependencies.groovy
@@ -16,6 +16,8 @@ class Dependencies {
   static def springBootStarterWeb = { it=Versions.springBoot -> "org.springframework.boot:spring-boot-starter-web:${it}" }
   static def springDataSolr = { it=Versions.springData -> "org.springframework.data:spring-data-solr:${it}" }
   static def transformAPI = { it=Versions.transformAPI -> "com.connexta.transformation:transformation-api-rest-spring-stubs:${it}" }
+  static def springActuator = { it=Versions.springBoot -> "org.springframework.boot:spring-boot-starter-actuator:${it}" }
+
 
   //  Test Dependencies
   static def hamcrestOptional = { it=Versions.npathai -> "com.github.npathai:hamcrest-optional:${it}" }
