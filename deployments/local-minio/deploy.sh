91d5537010e9ce1c676b46b5d83b28d549568ab1
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-07-15 11:30:54 -0700

    Integrated with docker override changes

--- deployments/local-minio/deploy.sh
+++ deployments/local-minio/deploy.sh
@@ -9,11 +9,22 @@ Utility script for setting up a docker network and deploying a local instance of
 'deploy' is the default option if no command is given.
 
 Commands:
-  d, deploy\t\tcreate the docker network and stack, stopping if one already exists
+  d, deploy\t\tcreate the docker network and stack, stopping if either already exists
   u, update\t\tattempts an in-place update of the deployed docker stack
   c, clean\t\tremoves the deployed docker stack and network (destructive)
   r, redeploy\t\tclean + deploy: completely removes the stack and network and deploys new instances (destructive)
 "
+
+# Evaluate the first parameter passed to the function every periodically.
+# When the expression evaluates to an empty string, return.
+function wait_for_empty_results() {
+  sleep 1
+  while [[ -n  $(eval $1) ]]; do
+    sleep 1
+    printf "." >&2
+  done
+}
+
 
 # Removes the docker stack and network.
 # Docker networks usually take a few seconds to go down, so it waits to prevent redeployment issues
@@ -21,37 +32,30 @@ function cleanup () {
     printf "Removing $name docker stack and network...\n"
     docker stack rm $name
     docker network rm $name
-    waiting=$(docker network ls | grep "$name")
     printf "\nWaiting for docker network '$name' to go down."
-    while [ ! -z "$waiting" ]; do
-        sleep 2
-        printf "."
-        waiting=$(docker network ls | grep " $name ")
-    done
+    wait_for_empty_results "docker network ls | grep -w $name"
     printf "\nDone!\n"
+}
+
+function assert_stack_is_not_deployed() {
+    exists=$(docker stack ls | grep "$name")
+    if [ ! -z "$exists" ]; then
+        printf "[ERROR] '$name' stack already exists. Please run script with 'clean', 'redeploy', or 'help' for more info.\n"
+        exit 1
+    fi
 }
 
 # Waits for everything to start up
 function wait_for_containers () {
-    waiting=$(docker service ls | grep ""$name"_.*0/1 ")
     printf "\nWaiting for docker services to start (safe to exit)."
-    while [ ! -z "$waiting" ]; do
-        sleep 2
-        printf "."
-        waiting=$(docker service ls | grep ""$name"_.*0/1 ")
-    done
+    wait_for_empty_results "docker service ls | grep $name_.*0/[[:digit:]]+"
     printf "\nDone!\n"
 }
 
 # Deploy docker-compose.yml file via docker stacks
 function deploy_images () {
     printf "\nDeploying Docker Images...\n"
-    exists=$(docker stack ls | grep "$name")
-    if [ ! -z "$exists" ]; then
-        printf "[ERROR] '$name' stack already exists. Please run script with 'clean', 'redeploy', or 'help' for more info.\n"
-        exit 1
-    fi
-    docker stack deploy -c docker-compose.yml $name
+    docker stack deploy -c ../../docker-compose.yml $name
 }
 
 # Create attachable overlay docker network
@@ -94,6 +98,7 @@ else
     deploy | d)
         print_warning
         configure_network
+        assert_stack_is_not_deployed
         deploy_images
         wait_for_containers
         ;;
