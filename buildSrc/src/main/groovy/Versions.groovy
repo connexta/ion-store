9abf6248ba71dd81d208432dbfe13a896f7bc8fe
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-07-15 13:20:37 -0700

    Fix up some git rebase/merge issues

--- buildSrc/src/main/groovy/Versions.groovy
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -15,10 +15,10 @@ class Versions {
     static String commonsIO = "2.6"
     static String ingestAPI = "0.1.1"
     static String lombok = "1.18.8"
+    static String reactiveStreams = "1.0.2"
     static String springBoot = "2.1.6.RELEASE"
     static String springData = "4.0.9.RELEASE"
     static String transformAPI = "0.1.0"
-    static String reactiveStreams = "1.0.2"
 
     //  Test Versions
     static String junit = "4.12"
