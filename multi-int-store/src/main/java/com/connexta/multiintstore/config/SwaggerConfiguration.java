1418a52c3b9dccbaebb737328835b3cef9b2e357
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-07-15 20:49:13 -0700

    Fix dependencies

--- multi-int-store/src/main/java/com/connexta/multiintstore/config/SwaggerConfiguration.java
+++ multi-int-store/src/main/java/com/connexta/multiintstore/config/SwaggerConfiguration.java
@@ -6,11 +6,12 @@
  */
 package com.connexta.multiintstore.config;
 
+import static springfox.documentation.spi.DocumentationType.*;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -20,7 +21,7 @@ public class SwaggerConfiguration {
 
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
+    return new Docket(SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
