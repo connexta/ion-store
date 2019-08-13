0d020375766f2ec4bd4841de9bc2d15ee64a7bd5
Emily Berk <emily.m.berk@gmail.com>
2019-08-13 14:08:25 -0700

    Updated storeMetadata to index

--- multi-int-store/src/main/java/com/connexta/multiintstore/services/api/StoreApi.java
+++ multi-int-store/src/main/java/com/connexta/multiintstore/services/api/StoreApi.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,16 +28,5 @@ public interface StoreApi {
       method = RequestMethod.POST)
   ResponseEntity<Void> storeProduct(
       @RequestHeader(value = "Accept-Version") @NotBlank final String acceptVersion,
-      @Valid @RequestPart("file") @NotNull final MultipartFile file);
-
-  @RequestMapping(
-      value = "/product/{productId}/{metadataType}",
-      produces = {"application/json"},
-      consumes = {"multipart/form-data"},
-      method = RequestMethod.PUT)
-  ResponseEntity<Void> storeMetadata(
-      @RequestHeader(value = "Accept-Version") @NotBlank final String acceptVersion,
-      @PathVariable(value = "productId") @NotBlank final String productId,
-      @PathVariable(value = "metadataType") @NotBlank final String metadataType,
       @Valid @RequestPart("file") @NotNull final MultipartFile file);
 }
