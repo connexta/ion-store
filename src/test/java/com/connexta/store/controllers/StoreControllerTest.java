84b52215b69dbe762bf8844e05f9cb4e6d24df15
Peter Huffer <peter.huffer@connexta.com>
2019-11-26 12:54:17 -0700

    Fix some tests after rebase

--- src/test/java/com/connexta/store/controllers/StoreControllerTest.java
+++ src/test/java/com/connexta/store/controllers/StoreControllerTest.java
@@ -14,49 +14,16 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import com.connexta.ingest.rest.spring.IngestApi;
 import com.connexta.store.exceptions.QuarantineException;
-import com.connexta.store.rest.spring.StoreApi;
 import com.connexta.store.service.api.StoreService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
 // TODO: These test can be changed to test the MultipartFileValidator instead of the controller.
 @ExtendWith(MockitoExtension.class)
 public class StoreControllerTest {
-
-  private static final String STORE_API_VERSION = "testStoreApiVersion";
-
-  @Mock private StoreService mockStoreService;
-
-  private StoreApi storeApi;
-
-  private IngestApi ingestApi;
-
-  @BeforeEach
-  public void beforeEach() {
-    storeApi = new StoreController(mockStoreService, STORE_API_VERSION);
-    ingestApi = new StoreController(mockStoreService, STORE_API_VERSION);
-  }
-
-  @Test
-  public void testGetRequest() {
-    assertThat(storeApi.getRequest().isEmpty(), is(true));
-    assertThat(ingestApi.getRequest().isEmpty(), is(true));
-  }
-
-  @Test
-  void testCreateDatasetNotImplemented(@Mock MultipartFile mockMultipartFile) {
-    assertThat(
-        storeApi.createDataset(STORE_API_VERSION, mockMultipartFile).getStatusCode(),
-        is(HttpStatus.NOT_IMPLEMENTED));
-  }
 
   @Test
   void testQuarantineSuccess() {
